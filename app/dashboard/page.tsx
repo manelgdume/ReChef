977b6c33cc7f0b16cc2c3d1ed3e635d6c0ae5589
manelgdume <142036063+manelgdume@users.noreply.github.com>
2024-10-19 18:26:25 +0200

    solve esl

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -50,10 +50,7 @@ interface Ingredient{
   ingredient:string;
   quantity:string;
 }
-
-export const description =
-  "An orders dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. The main area has a list of recent orders with a filter and export button. The main area also has a detailed view of a single order with order details, shipping information, billing information, customer information, and payment information."
-
+ 
 export default function Dashboard() {
   const { userId } = useAuth();
   const [loading, setLoading] = useState<boolean>(true);
